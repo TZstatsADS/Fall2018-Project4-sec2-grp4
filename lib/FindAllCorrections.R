e371117a9d1055765c15dcbfd7da9d8ed6edc684
wanghouyaoleyao <31977693+wanghouyaoleyao@users.noreply.github.com>
2018-11-27 13:53:07 -0500

    update

--- lib/FindAllCorrections.R
+++ lib/FindAllCorrections.R
@@ -1,9 +1,11 @@
 ###############Find all the proposed correction candidates for every single word
+##input: a word to correct
+## output: all the proposed correct candidates
 
-source("../lib/InsertedCandidate.R")
-source("../lib/DeletionDatabase.R")
-source("../lib/SubstitionDatabase.R")
-source("../lib/SearchINDatabase.R")
+source("InsertedCandidate.R")
+source("DeletionDatabase.R")
+source("SubstitionDatabase.R")
+source("SearchINDatabase.R")
 
 
 AllCorrectionCandidates <- function(word)
--- lib/SearchINDatabase.R
+++ lib/SearchINDatabase.R
@@ -10,12 +10,19 @@ library(dplyr)
 library(stringdist)
 SearchINCandidate <- function(word,database){
   candidate <- list()
+  output <- list()
   for (i in 1:length(database)){
     for (j in 1:length(database[[i]])){
-      candidate <- append(candidate,database[[i]][[j]]$correction[is.element(word,database[[i]][[j]]$typo)])
+      C <- database[[i]][[j]]$correction[is.element(word,database[[i]][[j]]$typo)]
+      X <- database[[i]][[j]]$X[word==database[[i]][[j]]$typo]
+      Y <- database[[i]][[j]]$Y[is.element(word,database[[i]][[j]]$typo)]
+      candidate <- append(candidate,c(C,X,Y))
     }
   }
-  return(candidate)
+  for(k in 1:(length(candidate)/3)){
+    output[[k]] <- list(corr=candidate[[3*k-2]],X=candidate[[3*k-1]],Y=candidate[[3*k]])
+  }
+  return(output)
 }
 
 
