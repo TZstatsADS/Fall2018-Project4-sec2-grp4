75e00f0df5a2f3ab9c2a253d77044279103478aa
lujia wang <lw2772@columbia.edu>
2018-11-28 23:31:23 -0500

    update main
    add svm train

--- lib/svm.R
+++ lib/svm.R
@@ -1,25 +1,29 @@
+#library(caret)
+
+# #Defining the groupname in the data column
+# dat <- dat %>% mutate(group_num = case_when(file_num %in% c(1:10)~"A",
+#                                             file_num %in% c(11:38)~"B",
+#                                             file_num %in% c(39:41)~"C",
+#                                             file_num %in% c(42:68)~"D",
+#                                             file_num %in% c(69:100)~"E"
+# ))
+# 
+# # Changing the levels of the labels
+# dat <- dat %>% mutate(label = case_when(label == 1 ~"Correct",
+#                                         label == 0 ~"Error"))
+# 
+# # Converting labels to factors
+# dat$label <- factor(dat$label)
+# 
+##############################
+## svm.func
+## Input: a data frame containing features, labels, and file number
+## Output: a list of prediction labels and accuracy
+##############################
 library(caret)
+library(dplyr)
 
-#Defining the groupname in the data column
-dat <- dat %>% mutate(group_num = case_when(file_num %in% c(1:10)~"A",
-                                            file_num %in% c(11:38)~"B",
-                                            file_num %in% c(39:41)~"C",
-                                            file_num %in% c(42:68)~"D",
-                                            file_num %in% c(69:100)~"E"
-))
-
-# Changing the levels of the labels
-dat <- dat %>% mutate(label = case_when(label == 1 ~"Correct",
-                                        label == 0 ~"Error"))
-
-# Converting labels to factors
-dat$label <- factor(dat$label)
-
-
-#SVM Functions
 svm.func <- function(dat){
-  library(caret)
-  
   # defining the radial grid with the two hyperparameters to be tuned
   grid_radial <- expand.grid(sigma = c(0.25, 1), C = c(1,10))
   
@@ -51,25 +55,25 @@ svm.func <- function(dat){
   
 }
 
-# Splitting the dataset by groups
-groupsplit <- split(dat,dat$group_num)
-
-# Results
-a <- svm.func(groupsplit$A)
-b <- svm.func(groupsplit$B)
-c <- svm.func(groupsplit$C)
-d <- svm.func(groupsplit$D)
-e <- svm.func(groupsplit$E)
-
-#Saving the outputs
-abcd <- rbind(matrix(a$pred),matrix(b$pred),matrix(c$pred),matrix(d$pred))
-abcd.list <- list(abcd)
-
-# Saving the results
-save(abcd, file = "../output/tempSvmLabels.RData")
-save(abcd.list, file = "../output/listtempSvmLabels.RData")
-
-confs <- cbind(a$conf$overall[1],b$conf$overall[1],c$conf$overall[1],d$conf$overall[1])
-colnames(confs) <- c("Group 1","Group 2","Group 3","Group 4")
-
-save(confs, file = "../output/accuracy.RData")
+# # Splitting the dataset by groups
+# groupsplit <- split(dat,dat$group_num)
+# 
+# # Results
+# a <- svm.func(groupsplit$A)
+# b <- svm.func(groupsplit$B)
+# c <- svm.func(groupsplit$C)
+# d <- svm.func(groupsplit$D)
+# e <- svm.func(groupsplit$E)
+# 
+# #Saving the outputs
+# abcd <- rbind(matrix(a$pred),matrix(b$pred),matrix(c$pred),matrix(d$pred))
+# abcd.list <- list(abcd)
+# 
+# # Saving the results
+# save(abcd, file = "../output/tempSvmLabels.RData")
+# save(abcd.list, file = "../output/listtempSvmLabels.RData")
+# 
+# confs <- cbind(a$conf$overall[1],b$conf$overall[1],c$conf$overall[1],d$conf$overall[1])
+# colnames(confs) <- c("Group 1","Group 2","Group 3","Group 4")
+# 
+# save(confs, file = "../output/accuracy.RData")
